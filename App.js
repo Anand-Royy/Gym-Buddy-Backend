43286d7460847115345522a44d1ba3efef38e48b
Anand Roy <73389636+Anand-Royy@users.noreply.github.com>
2024-09-15 01:06:55 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -9,47 +9,47 @@ app.use(express.json());
 app.use(cors());
 dotenv.config();
 
-app.post(
-  'https://gym-buddy-backend-zeta.vercel.app/api/routines',
+app.post('https://gym-buddy-sage.vercel.app/api/routines', async (req, res) => {
+  try {
+    const { length, name, fields } = req.body; // Get data from the request body
+    const id = length;
+    const exercises = fields;
+    const routineName = name;
+    console.log(req.body);
+    console.log(id);
+    console.log(exercises);
+    console.log(routineName);
+    const newRoutine = new Routine({
+      id,
+      exercises,
+      routineName,
+    });
+    const r = await newRoutine.save();
+    console.log(r);
+    res.status(201).json({ success: true, data: newRoutine });
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+});
+
+app.get(
+  'https://gym-buddy-sage.vercel.app/api/routines/:id',
   async (req, res) => {
+    const { id } = req.params;
+    console.log(id);
     try {
-      const { length, name, fields } = req.body; // Get data from the request body
-      const id = length;
-      const exercises = fields;
-      const routineName = name;
-      console.log(req.body);
-      console.log(id);
-      console.log(exercises);
-      console.log(routineName);
-      const newRoutine = new Routine({
-        id,
-        exercises,
-        routineName,
-      });
-      const r = await newRoutine.save();
-      console.log(r);
-      res.status(201).json({ success: true, data: newRoutine });
-    } catch (error) {
-      res.status(400).json({ success: false, error: error.message });
+      const routine = await Routine.findOne({ id });
+      if (!routine) {
+        return res.status(404).json({ message: 'Routine not found' });
+      }
+      res.status(200).json(routine);
+    } catch (err) {
+      res.status(500).json({ message: 'Server error', error: err.message });
     }
   }
 );
-
-app.get('/api/routines/:id', async (req, res) => {
-  const { id } = req.params;
-  console.log(id);
-  try {
-    const routine = await Routine.findOne({ id });
-    if (!routine) {
-      return res.status(404).json({ message: 'Routine not found' });
-    }
-    res.status(200).json(routine);
-  } catch (err) {
-    res.status(500).json({ message: 'Server error', error: err.message });
-  }
-});
 // Example route to get all routines
-app.get('/api/routines', async (req, res) => {
+app.get('https://gym-buddy-sage.vercel.app/api/routines', async (req, res) => {
   try {
     const routines = await Routine.find();
     res.status(200).json({ success: true, data: routines });
